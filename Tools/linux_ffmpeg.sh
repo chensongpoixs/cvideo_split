40717cd441684ccd8df4d0c7e72d19d016dac646
chensong <15850774503@163.com>
2024-03-04 15:48:54 +0800

    [linux上ffmpeg编译文档]

---
+++ Tools/linux_ffmpeg.sh
@@ -0,0 +1,7 @@
+﻿
+
+
+# avfilter drawtext 模块编译  --enable-libfreetype   --enable-libfontconfig  --enable-libfribidi --enable-libharfbuzz  
+# --enable-static   --enable-gpl   --enable-cross-compile
+
+ ./configure     --enable-version3  --enable-cuda --enable-cuvid --enable-nvdec --enable-nvenc   --enable-avfilter  --enable-libx264    --enable-libfreetype   --enable-libfontconfig  --enable-libfribidi --enable-libharfbuzz  --enable-static   --enable-gpl   --enable-cross-compile  --extra-cflags="-I/home/pc/Work/nv-codec-headers -I/usr/local/include"  --extra-ldflags="-L/usr/lib -L/usr/local/lib" --pkg-config="pkg-config --static"
