2f7daf3f43fe0ef0e23fa3602d09d1e75ddf99c1
chensong <15850774503@163.com>
2024-03-04 16:47:39 +0800

    [解码问题]

--- web_api/springboot-knife4j/src/main/java/cn/edu/sgu/www/knife4j/SpringbootKnife4j.java
+++ web_api/springboot-knife4j/src/main/java/cn/edu/sgu/www/knife4j/SpringbootKnife4j.java
@@ -19,24 +19,8 @@ public class SpringbootKnife4j {
     public static void main(String[] args)
     {
         if (logger.isDebugEnabled()) {
-            logger.debug("启动springboot整合knife4j案例项目...");
+            logger.debug(" Execl Tool Server ");
         }
-//        File file = new File("D:\\Work\\cai\\cvideo_split\\res\\execl\\test_read_camera.xlsx");
-//        List<ArrayList<String>> list = null;
-//        try {
-//            list = ExeclUtils.readExcel(file);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        for (int i = 0; i < list.size(); i++) {
-//            //第一行全部数据
-//            List list1=list.get(i);
-//            for (int j = 0; j < list1.size(); j++) {
-//                //第一行每个单元格数据
-//                System.out.println(list1.get(j));
-//            }
-//        }
-
         SpringApplication.run(SpringbootKnife4j.class, args);
     }
 
