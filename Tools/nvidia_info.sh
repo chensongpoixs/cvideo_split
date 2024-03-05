16231b6b4963ff6c1890cd1b751aaa1cde270e26
chensong <15850774503@163.com>
2024-03-05 14:31:40 +0800

    [gpu显卡]

--- Tools/nvidia_info.sh
+++ Tools/nvidia_info.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 
 for num in {1..100000}  
 do  
 	clear
 	echo $num  
+	nvidia-smi  --list-gpus
 	
 	nvidia-smi -i 0 -q -d UTILIZATION
 #	nvidia-smi -i 1 -q -d UTILIZATION
