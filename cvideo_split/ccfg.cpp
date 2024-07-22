3e286f7f57e52667bb23d9644161539deac7d07e
chensong <15850774503@163.com>
2024-07-22 09:43:51 +0800

    [默認open ffmpeg]

--- cvideo_split/ccfg.cpp
+++ cvideo_split/ccfg.cpp
@@ -63,7 +63,7 @@ namespace chen {
 		set_uint32(ECI_MediaOpenTimeOut, "media_open_time_out", 1000);
 		set_uint32(ECI_MediaReadTimeOut, "media_read_time_out", 1000);
 		set_uint32(ECI_UseGpuMaxCount, "use_gpu_max_count", 8);
-		set_uint32(ECI_OpenFfmpegMpegts, "open_ffmpeg_mpegts", 0);
+		set_uint32(ECI_OpenFfmpegMpegts, "open_ffmpeg_mpegts", 1);
 		set_uint32(ECI_CheckCameraStatus, "check_camera_status", 60 * 60 );
 		set_string(ECI_AuthPass, "auth_pass", "chensong");
 		//é…ç½®è¡¨çš„å€¼
