d523c2965b2e776f34a7bab0cae08b8c156556f4
chensong <15850774503@163.com>
2024-09-18 16:16:41 +0800

    [内存泄漏]

--- cvideo_split/cdecode.cpp
+++ cvideo_split/cdecode.cpp
@@ -421,7 +421,7 @@ namespace chen {
 			//}
 			if (ret < 0)
 			{
-				//	av_packet_unref(m_packet_ptr);
+				 	av_packet_unref(m_packet_ptr);
 				std::thread::id thread_id = std::this_thread::get_id();
 				std::ostringstream cmd;
 				cmd << thread_id;
@@ -526,7 +526,7 @@ namespace chen {
 			if (ret == AVERROR_EOF || avio_feof(m_ic_ptr->pb))
 			{
 				::avcodec_flush_buffers(m_codec_ctx_ptr);
-				//_stop_callback();
+				_stop_callback();
 				//::av_frame_unref(m_picture_ptr);
 				std::thread::id thread_id = std::this_thread::get_id();
 				std::ostringstream cmd;
